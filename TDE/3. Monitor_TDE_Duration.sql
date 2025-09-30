999f0712797ee94a35cb5d8d547902cf8f218a6a
Rohit Kumar <31687594+rohitagio@users.noreply.github.com>
2025-09-30 13:49:41 -0600

    Update 3. Monitor_TDE_Duration.sql

--- TDE/3. Monitor_TDE_Duration.sql
+++ TDE/3. Monitor_TDE_Duration.sql
@@ -1,33 +1,28 @@
 
 --Step 13: Polling to monitor the progress of encryption (Run in a different window)
 DECLARE @state tinyint;
-DECLARE @encyrption_progress 
+DECLARE @TDE_encyrption_progress 
     TABLE(sample_time DATETIME, percent_complete DECIMAL(5,2))
 
-SELECT @state = k.encryption_state
-FROM sys.dm_database_encryption_keys k
-INNER JOIN sys.databases d
-   ON k.database_id = d.database_id
-WHERE d.name = 'TestTDE';
+SELECT @state = k.encryption_state FROM sys.dm_database_encryption_keys k
+INNER JOIN sys.databases d    ON k.database_id = d.database_id
+WHERE d.name = 'TDE_Demo';
 
 WHILE @state != 3
 BEGIN
-   INSERT INTO @encyrption_progress(sample_time, percent_complete)
-   SELECT GETDATE(), percent_complete
-   FROM sys.dm_database_encryption_keys k
-   INNER JOIN sys.databases d
-      ON k.database_id = d.database_id
-   WHERE d.name = 'TestTDE';
+   INSERT INTO @TDE_encyrption_progress(sample_time, percent_complete)
+   SELECT GETDATE(), percent_complete FROM sys.dm_database_encryption_keys k
+   INNER JOIN sys.databases d ON k.database_id = d.database_id
+   WHERE d.name = 'TDE_Demo';
 
 
-   WAITFOR delay '00:00:05';
+   WAITFOR delay '00:00:10';
 
-   SELECT @state = k.encryption_state
-   FROM sys.dm_database_encryption_keys k
-   INNER JOIN sys.databases d
-      ON k.database_id = d.database_id
-   WHERE d.name = 'TestTDE'; 
+   SELECT @state = k.encryption_state FROM sys.dm_database_encryption_keys k
+   INNER JOIN sys.databases d ON k.database_id = d.database_id
+   WHERE d.name = 'TDE_Demo'; 
 END
 
-SELECT * FROM @encyrption_progress;
+SELECT * FROM @TDE_encyrption_progress;
+
 GO
