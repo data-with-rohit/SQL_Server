338251f66cf58c785c217123204ac55d80d6b560
Rohit Kumar <31687594+rohitagio@users.noreply.github.com>
2025-09-30 13:48:04 -0600

    Update 2. Create_TDE.sql

--- TDE/2. Create_TDE.sql
+++ TDE/2. Create_TDE.sql
@@ -2,12 +2,12 @@
 USE master;
 GO
 CREATE MASTER KEY
-ENCRYPTION BY PASSWORD = 'UseAStrongPasswordHere!£$7';
+ENCRYPTION BY PASSWORD = 'UseAStrongPasswordHere!@#$%9';
 GO
 
 --Step 2: Backing up the DMK
 BACKUP MASTER KEY TO FILE = 'C:\Test\MyDMK'   
-ENCRYPTION BY PASSWORD = 'UseAnotherStrongPasswordHere!£$7';
+ENCRYPTION BY PASSWORD = 'UseAStrongPasswordHere!@#$%9345';
 GO
 
 --Step 3: Creating the Certificate
@@ -25,7 +25,7 @@ TO FILE = 'C:\Test\MyTDECert.cer'
 WITH PRIVATE KEY   
 (  
     FILE = 'C:\Test\MyTDECert_PrivateKeyFile.pvk',  
-    ENCRYPTION BY PASSWORD = 'UseAStrongPasswordHereToo!£$7'  
+    ENCRYPTION BY PASSWORD = 'UseAStrongPasswordHere!@#$%9ASdedf'  
 );
 GO
 
@@ -101,42 +101,5 @@ GO 350
 ALTER DATABASE TDE_Demo SET ENCRYPTION ON;
 GO
 
---Step 13: Polling to monitor the progress of encryption (Run in a different window)
-DECLARE @state tinyint;
-DECLARE @encyrption_progress 
-    TABLE(sample_time DATETIME, percent_complete DECIMAL(5,2))
 
-SELECT @state = k.encryption_state
-FROM sys.dm_database_encryption_keys k
-INNER JOIN sys.databases d
-   ON k.database_id = d.database_id
-WHERE d.name = 'TDE_Demo';
-
-WHILE @state != 3
-BEGIN
-   INSERT INTO @encyrption_progress(sample_time, percent_complete)
-   SELECT GETDATE(), percent_complete
-   FROM sys.dm_database_encryption_keys k
-   INNER JOIN sys.databases d
-      ON k.database_id = d.database_id
-   WHERE d.name = 'TDE_Demo';
-
-
-   WAITFOR delay '00:00:05';
-
-   SELECT @state = k.encryption_state
-   FROM sys.dm_database_encryption_keys k
-   INNER JOIN sys.databases d
-      ON k.database_id = d.database_id
-   WHERE d.name = 'TDE_Demo'; 
-END
-
-SELECT * FROM @encyrption_progress;
-GO
-
---Step 14: Checking for blocking caused by encryption
-SELECT *
-FROM sys.dm_tran_locks
-WHERE resource_type = 'ENCRYPTION_SCAN';
-GO
 
